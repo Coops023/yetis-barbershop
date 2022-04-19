67bf3c1ee75ce11eecc9c02c187f3ac754a97ab8
Coops023 <bjorkc27@gmail.com>
2022-04-19 20:55:42 +1000

    styling on contact form

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -7,6 +7,7 @@ export default function ContactUs() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
+  const [modal, setModal] = useState(false);
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -33,41 +34,89 @@ export default function ContactUs() {
       setName("");
       setEmail("");
       setMessage("");
+      setModal(true);
     }
   };
-
   return (
-    <form className="contact-form" ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input
-        value={name}
-        onChange={function handleChange(e) {
-          setName(e.target.value);
-        }}
-        type="text"
-        name="user_name"
-        required={true}
-      />
-      <label>Email</label>
-      <input
-        value={email}
-        onChange={function handleChange(e) {
-          setEmail(e.target.value);
-        }}
-        type="email"
-        name="user_email"
-        required={true}
-      />
-      <label>Message</label>
-      <textarea
-        value={message}
-        onChange={function handleChange(e) {
-          setMessage(e.target.value);
-        }}
-        name="message"
-        required={true}
-      />
-      <input type="submit" value="Send" />
-    </form>
+    <div>
+      {!modal ? (
+        <form className="contact-form" ref={form} onSubmit={sendEmail}>
+          <label>Name</label>
+          <input
+            className="input"
+            value={name}
+            onChange={function handleChange(e) {
+              setName(e.target.value);
+            }}
+            type="text"
+            name="user_name"
+            required={true}
+            placeholder="Steve Smith"
+          />
+          <label>Email</label>
+          <input
+            className="input"
+            value={email}
+            onChange={function handleChange(e) {
+              setEmail(e.target.value);
+            }}
+            type="email"
+            name="user_email"
+            required={true}
+            placeholder="eg. steve.smith@gmail.com"
+          />
+          <label>Message</label>
+          <textarea
+            className="input"
+            value={message}
+            onChange={function handleChange(e) {
+              setMessage(e.target.value);
+            }}
+            name="message"
+            required={true}
+            placeholder="Leave your message here..."
+          />
+          <button className="send-button" type="submit">
+            Send
+          </button>
+        </form>
+      ) : (
+        <form className="contact-form" ref={form} onSubmit={sendEmail}>
+          <div className="success-modal">Thank you for getting in touch.</div>
+          <label>Name</label>
+          <input
+            value={name}
+            onChange={function handleChange(e) {
+              setName(e.target.value);
+            }}
+            type="text"
+            name="user_name"
+            required={true}
+          />
+          <label>Email</label>
+          <input
+            value={email}
+            onChange={function handleChange(e) {
+              setEmail(e.target.value);
+            }}
+            type="email"
+            name="user_email"
+            required={true}
+          />
+          <label>Message</label>
+          <textarea
+            value={message}
+            onChange={function handleChange(e) {
+              setMessage(e.target.value);
+            }}
+            name="message"
+            required={true}
+          />
+          <button className="send-button" type="submit">
+            Send
+          </button>
+        </form>
+      )}
+    </div>
   );
 }
